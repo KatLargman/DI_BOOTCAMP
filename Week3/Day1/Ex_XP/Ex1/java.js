8e705055bc91773d513512e553e1c45726f2dc25
KatLargman <86594834+KatLargman@users.noreply.github.com>
2021-07-11 22:13:57 +0300

    Update java.js

--- Week3/Day1/Ex_XP/Ex1/java.js
+++ Week3/Day1/Ex_XP/Ex1/java.js
@@ -1,6 +1,13 @@
-let div = document.getElementById('navBar');
-div.setAttribute('id', 'socialNetworkNavigation');
-let li = document.createElement('li');
-let text = document.createTextNode('Logout');
-li.appendChild(text);
-document.querySelector('ul').appendChild(li);
+
+let divId = document.querySelector("div"); //1
+divId.setAttribute("id", "socialNetworkNavigation");
+console.log(divId);
+let newList = document.createElement("li"); //2.1
+let node = document.createTextNode("Logout"); //2.2
+newList.appendChild(node); //2.3
+let list = document.querySelector("ul"); //2.4
+list.appendChild(newList);
+let firstList = list.firstElementChild.textContent; //3
+console.log(firstList);
+let lastList = list.lastElementChild.textContent;
+console.log(lastList);
