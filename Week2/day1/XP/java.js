2a1785bfdeef68c712aaa223f85b2e3a3288a5dc
KatLargman <86594834+KatLargman@users.noreply.github.com>
2021-07-21 22:41:51 +0300

    Create java.js

---
+++ Week2/day1/XP/java.js
@@ -0,0 +1,41 @@
+
+console.log("results:");
+
+let a = 5 + "34";
+console.log(a);
+
+let b = 5 - "4";
+console.log(b);
+
+let c = 10 % 5;
+console.log(c);
+
+let d = 5 % 10;
+console.log(d);
+
+let e = "Java" + "Script";
+console.log(e);
+
+let f = " " + " ";
+console.log(f);
+
+let g = " " + 0;
+console.log(g);
+
+let h = true + true;
+console.log(h);
+
+let i = true + false;
+console.log(i);
+
+let j = false + true;
+console.log(j);
+
+let k = false - true;
+console.log(k);
+
+let l = 3 - 4;
+console.log(l);
+
+let m = "Bob" - "bill";
+console.log(m);
