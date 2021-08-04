d5f3ec20455af3b611bd88b674dae3d3edfa8878
KatLargman <86594834+KatLargman@users.noreply.github.com>
2021-08-04 23:45:03 +0300

    Create java.js

---
+++ week5/day1/xp/ex3/java.js
@@ -0,0 +1,29 @@
+let marioGame = {
+  detail: "An amazing game!",
+  characters: {
+    mario: {
+      description: "Small and jumpy. Likes princesses.",
+      height: 10,
+      weight: 3,
+      speed: 12,
+    },
+    bowser: {
+      description: "Big and green, Hates princesses.",
+      height: 16,
+      weight: 6,
+      speed: 4,
+    },
+    princessPeach: {
+      description: "Beautiful princess.",
+      height: 12,
+      weight: 2,
+      speed: 2,
+    },
+  },
+};
+
+let marioStringified = JSON.stringify(marioGame);
+console.log(marioStringified);
+
+let marioObjectified = JSON.parse(marioStringified);
+console.log(marioObjectified);
