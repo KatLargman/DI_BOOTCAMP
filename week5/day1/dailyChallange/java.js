e33d7a3ddc50dcad0eca998536c96ff2f54c3455
KatLargman <86594834+KatLargman@users.noreply.github.com>
2021-08-04 23:52:46 +0300

    Update java.js

--- week5/day1/dailyChallange/java.js
+++ week5/day1/dailyChallange/java.js
@@ -1,1 +1,16 @@
-java
+const getJson = () => {
+  console.log("fired");
+  let frm = document.forms.frm;
+  let firstName = frm.elements.firstName.value;
+  let lastName = frm.elements.lastName.value;
+  console.log(firstName, lastName);
+
+  let obj = {
+    firstName,
+    lastName,
+  };
+
+  let json = JSON.stringify(obj);
+  console.log(json);
+  return false;
+};
